fece3fe707b2c0f9c8908fa1de3d23e0bf255e25
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2022-11-30 16:28:40 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
   // database connection
-  $connection = mysqli_connect("ftp.main.geaa.tech","u180008257_CCompanion","CCompanion1PASS","u180008257_CCompanion");
+  $connection = mysqli_connect("ftp.main.geaa.tech","u180008257_CCompanion","","u180008257_CCompanion");
 
   if(!$connection){
     die("Connection failed: ".mysql_connect_error());
